c64bf6b599e8fb6f6140472f63b7e31d6715c6f9
Lindo <c@changry.no>
2020-10-26 20:43:13 +0100

    Added sort function for cards

--- cards.py
+++ cards.py
@@ -8,11 +8,12 @@ import string
 from copy import deepcopy
 from datetime import datetime
 
-token = "NDM2OTMyMTc2MjM2MzgwMTYw.WtoazA.uvIZlzRMu_CkDcrtUBzzNeOj2xE"
+token = ""
 client = discord.Client()
 
 VERSION = "0.0.0"
 
+SUITS = ["Diamonds","Hearts","Clubs","Spades"]
 CARDS = ["0","Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"]
 
 class card:
@@ -90,13 +91,19 @@ async def on_message(message):
     # COMMANDS #
 
     if message.content.lower() == ".durak":
+        # errors #
+        if not game is None:
+            await message.channel.send("A game has already started in this channel.")
+            return
+        # errors #
+
         new_game = {}
         new_game["owner_id"] = user.id
         new_game["channel_id"] = message.channel.id
         new_game["game_type"] = "Durak"
         new_game["start_time"] = ""
         new_game["deck"] = build_deck("Durak")
-        new_game["data"] = {"trump":"","attacker_id":0,"defender_id":0,}
+        new_game["data"] = {"trump":"","attacker":0,"cards":[],"attack_card":None}
         new_game["players"] = [{"player_id":user.id,"player_name":user.display_name,"hand":[]}]
         games.append(new_game)
         await message.channel.send(f"**Starting a game of Durak!**\nGame owner: {user.display_name}\nJoin with `.join`")
@@ -122,28 +129,37 @@ async def on_message(message):
         if game["start_time"] != "": # return error if game has already started
             await message.channel.send("Game has already started.")
             return
+        if game["owner_id"] != user.id:
+            await message.channel.send("You are not the owner of this game.")
+            return
         # errors #
 
         game["start_time"] = datetime.utcnow().__str__() + " UTC" # set start time
 
         if game["game_type"] == "Durak":
+            
             trump = game["deck"].pop() # assign trump
             game["data"]["trump"] = trump.suit
             await message.channel.send(f"{trump.suit} is trump!\n{trump.display()}")
             for p in range(len(game["players"])): # deal cards
                 draw(game["deck"], game["players"][p]["hand"], 6)
                 await user.create_dm()
+                sort(game["players"][p]["hand"])
                 for card_name in [x.display() for x in game["players"][p]["hand"]]:
                     bot_msg = await user.dm_channel.send(card_name)
                     await bot_msg.add_reaction(EMOJI["use"])
 
 
 
+def sort(cards):
+    cards.sort(key=lambda x: SUITS.index(x.suit)*100 - x.val)
+
+
 def build_deck(game):
     deck = []
     if game == "Durak" or game == "standard":
-        for suit in ["Clubs","Diamonds","Hearts","Spades"]:
-            for value in range(1 if game == "standard" else 6, 14):
+        for suit in SUITS:
+            for value in range(1 if game == "standard" else 6, 15):
                 new_card = card(value, suit)
                 deck.append(new_card)
     shuffle(deck)
